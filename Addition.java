73bbd78b82602e45073c1b886b8d0d1b06a6cd43
satish8234 <30949782+satish8234@users.noreply.github.com>
2024-06-21 13:19:09 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Addition
+{
+public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);
+	int a;
+	a=s.nextInt();
+	float b=Math.sqrt(a);
+	System.out.println(b);
+}
+}
